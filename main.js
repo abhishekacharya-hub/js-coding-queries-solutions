f758cff9c55cc8192c91064a207eca8a3533b329
abhishek acharya <85024420+abhishekacharya-hub@users.noreply.github.com>
2022-09-30 19:42:06 +0530

    firstcommit

---
+++ main.js
@@ -0,0 +1,15 @@
+//fizzbuzz solution
+function fizzBuzz(n) {
+    for(let i = 0; i<n+1; i++) {
+        if(i % 15 === 0) {
+            console.log("fizzbuzz");
+        }else if(i % 5 === 0) {
+            console.log("fizz");
+        }else if(i % 3 === 0) {
+            console.log("buzz");
+        }else {
+            console.log(i);
+        }
+    }
+}
+fizzBuzz(50);
